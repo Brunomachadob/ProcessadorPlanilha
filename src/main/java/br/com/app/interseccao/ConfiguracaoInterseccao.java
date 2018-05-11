3b54328ad02befaee5ef2f7aae7631a02159a2ad
Bruno Machado Brandão <brunin5010@gmail.com>
2018-05-11 17:26:59 -0300

    Adicionando sets nos objetos de configuração

--- src/main/java/br/com/app/interseccao/ConfiguracaoInterseccao.java
+++ src/main/java/br/com/app/interseccao/ConfiguracaoInterseccao.java
@@ -1,8 +1,16 @@
 package br.com.app.interseccao;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class ConfiguracaoInterseccao {
 	boolean temCabecalho;
-	ArrayList<String> colunas;
+	List<String> colunas;
+	
+	public void setTemCabecalho(boolean temCabecalho) {
+		this.temCabecalho = temCabecalho;
+	}
+
+	public void setColunas(List<String> colunas) {
+		this.colunas = colunas;
+	}
 }
--- src/main/java/br/com/app/processador/ConfiguracaoProcessamento.java
+++ src/main/java/br/com/app/processador/ConfiguracaoProcessamento.java
@@ -6,10 +6,29 @@ public class ConfiguracaoProcessamento {
 	public boolean temCabecalho;
 	public List<ConfiguracaoColuna> colunas;
 	
+	public void setTemCabecalho(boolean temCabecalho) {
+		this.temCabecalho = temCabecalho;
+	}
+	
+	public void setColunas(List<ConfiguracaoColuna> colunas) {
+		this.colunas = colunas;
+	}
+	
 	public static class ConfiguracaoColuna {
 		public String nome;
 		public String descricao;
 		public List<String> processadores;
+	
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
 		
+		public void setDescricao(String descricao) {
+			this.descricao = descricao;
+		}
+		
+		public void setProcessadores(List<String> processadores) {
+			this.processadores = processadores;
+		}
 	}
 }
